75bf493ff00e5e4c946ec96ae1113a55f8b12039
Ritajaritz <137432446+Ritajaritz@users.noreply.github.com>
2023-11-22 21:51:01 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -6,7 +6,7 @@ function Login(){
     return (
         <div className="background">
             <h1 id="header_login">
-                welcome to Solid-clock
+                Welcome To Employee Time Tracker
             </h1>
             <Login_box/>
         </div>
